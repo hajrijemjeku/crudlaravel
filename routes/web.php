234e268758bf456beb082e855feffcecfc7e04eb
hajrijemjeku <hajrijemj@gmail.com>
2025-04-27 00:25:31 +0200

    Defined resource routes for posts with PostController and homepage route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Livewire\Settings\Appearance;
-use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
+use App\Livewire\Settings\Password;
+use App\Livewire\Settings\Appearance;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -32,3 +33,6 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::resource('posts', PostController::class);
+Route::get('/homepage', [PostController::class, 'allposts'])->name('homepage');
