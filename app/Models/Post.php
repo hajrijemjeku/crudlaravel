6e40e404ae710f3c6bd041cd7bd9bc492581b28e
hajrijemjeku <hajrijemj@gmail.com>
2025-04-27 00:22:32 +0200

    Post Model added and user() method to define relationship between post and user implemented

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     //
-    protected $fillable = ['title', 'content'];
+    protected $fillable = ['title', 'content', 'user_id'];
+
+    //Define relationship between post and user (a post belongs to a user)
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
